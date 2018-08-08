a33f1091fd17e5eaa2a0e2fbea011856133d3993
Thibaut Valarche <thibautvalarche@gmail.com>
2018-08-08 16:49:44 +0200

    part8beststart

--- index.js
+++ index.js
@@ -366,24 +366,22 @@ app.get("*", checkLogin, (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 server.listen(8080); // it's server, not app, that does the listening
 
-io.on('connection', function(socket) {
-
+io.on("connection", function(socket) {
     if (!socket.request.session || !socket.request.session.user) {
-            return socket.disconnect(true);
-        }
-
+        return socket.disconnect(true);
+    }
     console.log(`socket with the id ${socket.id} is now connected`);
 
-    socket.on('disconnect', function() {
+    socket.on("disconnect", function() {
         console.log(`socket with the id ${socket.id} is now disconnected`);
     });
 
-    socket.on('thanks', function(data) {
+    socket.on("thanks", function(data) {
         console.log(data);
     });
 
-    socket.emit('welcome', {
-        message: 'Welome. It is nice to see you'
+    socket.emit("welcome", {
+        message: "Welome. It is nice to see you"
     });
 });
 
--- src/socket.js
+++ src/socket.js
@@ -1,30 +1,29 @@
-import * as io from 'socket.io-client';
+import * as io from "socket.io-client";
 //import { onlineUsers, userJoined, userLeft } from './actions';
 
-socket.on('welcome', function(data) {
+/*socket.on("welcome", function(data) {
     console.log(data);
-    socket.emit('thanks', {
-      	message: 'Thank you. It is great to be here.'
+    socket.emit("thanks", {
+        message: "Thank you. It is great to be here."
     });
-});
+});*/
 
-
+let socket;
 
 export function init(store) {
     if (!socket) {
         socket = io.connect();
 
-        socket.on('onlineUsers', users => {
-            store.dispatch(onlineUsers(users))
+        socket.on("onlineUsers", users => {
+            store.dispatch(onlineUsers(users));
         });
 
-        socket.on('userJoined', user => {
-            store.dispatch(userJoined(user))
-        })
+        socket.on("userJoined", user => {
+            store.dispatch(userJoined(user));
+        });
 
-        socket.on('userLeft', userId => {
-            store.dispatch(userLeft(userId))
-        })
+        socket.on("userLeft", userId => {
+            store.dispatch(userLeft(userId));
+        });
     }
-
 }
